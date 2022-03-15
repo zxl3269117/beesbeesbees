fff2651a21bc33ee727c5a4005336f1fc29eb452
Ziqian Li <liziqian1992@gmail.com>
2022-03-14 19:37:50 -0700

    Complete HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
 var HoneyMakerBee = function() {
+    Bee.call(this);
+    this.job = 'make honey';
+    this.age = 10;
+    this.honeyPot = 0;
+};
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+    this.honeyPot++;
 };
 
+HoneyMakerBee.prototype.giveHoney = function() {
+    this.honeyPot--;
+}
